e019bad03e4f57a5a884c7255c88c62da82b96c9
pog <208688662+Pogwat@users.noreply.github.com>
2026-02-08 04:59:51 -0500

    remove unreleated code and add comments

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,27 @@
-use std::os::unix::net::UnixStream;
-use std::env::var;
-use std::io::{BufRead, BufReader};
-
-use hashbrown::HashMap;
 mod shared;
 use shared::FastVec;
-
-use std::collections::BTreeMap;
 
 
 fn main() -> Result<(),std::io::Error> {
     let mut fv: FastVec<String> = FastVec::new();   
     //fv.insert(10,"hi".to_string());
-    println!("len:{}", fv.vector.len());
+    println!("len:{}", fv.vector.len()); //len:0
+
     fv.push("hi".to_string());
-    println!("hi is at: {}", fv.get_by_value(&"hi".to_string()).unwrap());
-    println!("0 is :{}", fv.get_by_key(0).unwrap());
+    println!("hi is at: {}", fv.get_by_value(&"hi".to_string()).unwrap()); //hi is at: 0
+    println!("0 is :{}", fv.get_by_key(0).unwrap()); //0 is :hi
 
     fv.insert(1, "hii".to_string()).unwrap();
-    println!("hii is at: {}", fv.get_by_value(&"hii".to_string()).unwrap());
+    println!("hii is at: {}", fv.get_by_value(&"hii".to_string()).unwrap()); //hii is at: 1
     
-    println!("old is {}, new is {}", fv.mod_by_key(1, "6767".to_string()).unwrap(), fv.get_by_key(1).unwrap());
+    println!("old is {}, new is {}", fv.mod_by_key(1, "6767".to_string()).unwrap(), fv.get_by_key(1).unwrap()); //old is hii, new is 6767
     
     for item in fv.iter() {
-        println!("{}",item);
+        println!("{}",item); //hi 6767
     }
-    println!("one is: {}", fv[1]);
-    println!("elements: {}", fv);
-    println!("full: {:?}", fv);
-    //let prr= fv.get_by_key(10).unwrap();
-    //println!("{}", prr);
-    std::process::exit(0);
-    //let vector: Vec<String> = Vec::new();
-        //let map = HashMap::new();
-        //let btreemap: BTreeMap<u8,Vec<usize>> = BTreeMap::new();
-   
-    //let fv = FastVec {vector,map,
-    //btreemap   
-//};
-    
-    println!("hello");
-    // Slabs | id, String | Entry n
-    // Hashmap| id, Entry -> Slab1.entry(n) & Slab2.entry(n)
-    let mut map: HashMap<String,(u8,u8,u8)> = HashMap::new();  //workspace, vector index, slab index
-
-    //for id sorting
-    let mut ordered_map: BTreeMap<u8,Vec<String>> = BTreeMap::new(); //workdspace and winids
-
-let sock: String =         format!(
-        "{}/hypr/{}/.socket2.sock",
-        var("XDG_RUNTIME_DIR").unwrap(),
-        var("HYPRLAND_INSTANCE_SIGNATURE").unwrap()
-    );
-    let stream = UnixStream::connect(sock).unwrap();
-    let reader = BufReader::new(stream);
-
-        for line in reader.lines() {
-            let line = line?; // Result<String, std::io::Error>
-     
-            if let Some((prefix, value)) = line.split_once(">>") {
-
-                match prefix {
-
-
-                    "openwindow" => { // openwindow>>55c018ac1aa0,3,kitty,kitty           
-                        let parts: Vec<&str> = value.split(',').collect();
-                        let [id, workspace, initialclass, initialtitle]: [&str; 4] = parts.try_into().expect("not 4 arguments in openwindow");            
-                        let workspace: u8 = workspace.parse().expect("workspace in openwindow is not u8");                    
-                        let (id,initialclass, initialtitle): (String,String,String) = (id.to_string(),initialclass.to_string(),initialtitle.to_string());
-                       
-
-                        //data.format();
-                    }
-
-                    "closewindow" => { // closewindow>>55c018ac1aa0    
-                        let id:&str = value;
-                        let (workspace,r_vec_index,slab_index) = map.remove(id).unwrap();
-                       
-
-
-                        //data.format();    
-                    }
-                
-                   _ => {}
-                
-                }
-
-
-
-
-
-            }
-
-        }
-    
-
+    println!("one is: {}", fv[1]); //one is: 6767
+    println!("elements: {}", fv); //elements: ["hi", "6767"]
+    println!("full: {:?}", fv); //full: FastVec { vector: ["hi", "6767"], map: {"6767": 1, "hi": 0} }
 
 Ok(()) 
 }
