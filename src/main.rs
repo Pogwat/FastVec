44de892acb08b6f1fbc0126a47338585c3907fdd
pog <208688662+Pogwat@users.noreply.github.com>
2026-03-04 00:18:23 -0500

    add some KeyVec stuff to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use testing::shared::FastVec;
 use testing::shared::Errors;
 use testing::shared::ValueMapKeyVec;
+
+//absoulute::Errors
+use testing::absoulute::KeyVec;
+use testing::absoulute::AbsoluteKeys;
+
 use testing::swap_refs;
 use core::ptr;
+
+
 
 fn main() -> Result<(),Errors>{
     let mut fv: FastVec<String> = FastVec::new();   
@@ -15,7 +22,7 @@ fn main() -> Result<(),Errors>{
     println!("vec before swap: {:?}",a);
     swap_refs!(&mut a[0], &mut a[2]);
     println!("vec after swap: {:?}",a);
-
+    absoulute();
 
     // let (old_v,_) = fv.mod_to(0, "3".to_string())?;
     // println!("{}",old_v);
@@ -32,6 +39,26 @@ fn main() -> Result<(),Errors>{
     // println!("capacity is {}",fv.capacity());
     // fv.reserve(12);
     // println!("new capacity is {}",fv.capacity());
+    Ok(())
+}
+
+fn absoulute() -> Result<(), testing::absoulute::Errors> {
+    let mut keys = KeyVec::new();
+    let mut values = vec![10,20,30,40,50];
+    values.iter().enumerate().for_each(|(k,v)|  {
+        keys.push_fake(k) ;
+        keys.push_reals_fake(k) ;
+    });
+    println!("{:?}", values);
+    println!("{:?}", keys);
+    println!("fake3 value:{}, fake2_real_key:{}", values[keys.get_fake(4)?.unwrap()], keys.get_fake(4)?.unwrap());
+    values.swap_remove(keys.get_fake(2)?.unwrap());
+    keys.remove_from_real_fake_by_fake(2);
+
+    println!("after swap: {:?}", values);
+    println!("after swap:  {:?}", keys);
+    println!("fake3 value:{}, fake2_real_key:{}", values[keys.get_fake(4)?.unwrap()], keys.get_fake(4)?.unwrap());
+    //The values in real fake refrences is the same
     Ok(())
 }
 
